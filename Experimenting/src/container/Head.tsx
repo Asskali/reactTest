1cb0063aab69b4bfe9c6f523de8e15538a84c356
Adam Asskali <adam.asskali@gmail.com>
2019-02-24 19:37:50 +0100

    Lagt til ikon for hvilken kategori man er i

--- Experimenting/src/container/Head.tsx
+++ Experimenting/src/container/Head.tsx
@@ -1,13 +1,14 @@
 /*tslint:disable-next-line*/
 import React from 'react';
-import { GridRow, Header, Menu, Icon } from 'semantic-ui-react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { GridRow, Header, Icon, Menu } from 'semantic-ui-react';
 
-interface IHead {
+interface IHead extends RouteComponentProps{
   title: string;
   sideMenu: any;
 }
 
-export default class Head extends React.Component<IHead, any> {
+class Head extends React.Component<IHead, any> {
 
   render() {
     return (
@@ -19,8 +20,33 @@ export default class Head extends React.Component<IHead, any> {
           <Menu.Item header className="adam-header">
             Adam's site
           </Menu.Item>
+          <Menu.Item header className="adam-header-right">
+            {this.currentIcon()}
+          </Menu.Item>
         </Menu>
       </div>
     );
   }
+
+  currentIcon = () => {
+    switch (this.props.location.pathname) {
+      case '/games': {
+        return  <Icon name="gamepad" color="red"/>;
+      }
+      case '/tvseries': {
+        return  <Icon name="tv" color="red"/>;
+      }
+      case '/anime': {
+        return  <Icon name="gripfire" color="red"/>;
+      }
+      case '/films': {
+        return  <Icon name="film" color="red"/>;
+      }
+      default: {
+        return  <Icon name="home" color="red"/>;
+      }
+    }
+  }
 }
+
+export default withRouter(Head);
