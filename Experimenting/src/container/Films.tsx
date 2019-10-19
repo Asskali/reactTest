c7bd251357c91348875e926e5c35f57072f4e03f
Adam Asskali <adam.asskali@gmail.com>
2019-10-19 19:27:29 +0200

    Bruker react-hooks i Films

--- Experimenting/src/container/Films.tsx
+++ Experimenting/src/container/Films.tsx
@@ -1,27 +1,17 @@
 /*tslint:disable-next-line*/
-import React from 'react';
+import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
 
-interface IFilmsProps {
-  title: string;
-  sideMenu: any;
-}
+export default function Films(props: any) {
+  const title = useContext(MyContext);
 
-export default class Films extends React.Component<IFilmsProps, any> {
-
-  static contextType = MyContext;
-  static title = 'Films';
-
-  componentDidMount = () => {
-    if (this.context.title !== Films.title) {
-      this.context.toggleTitle(Films.title);
-    }
+  if (title.title !== 'Films') {
+    title.toggleTitle('Films');
   }
-  render() {
-    return (
-      <div>
-          FILMS
-      </div>
-    );
-  }
+
+  return (
+    <div>
+        FILMS hooks!
+    </div>
+  );
 }
